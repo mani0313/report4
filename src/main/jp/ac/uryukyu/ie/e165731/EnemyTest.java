d9025fabd877945115708049decf39acf633de62
Aryu Shiroma <e165731@ie.u-ryukyu.ac.jp>
2016-11-28 16:09:24 +0900

    1

---
+++ src/main/jp/ac/uryukyu/ie/e165731/EnemyTest.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e165731;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165731 on 2016/11/28.
+ */
+public class EnemyTest {
+    @org.junit.Test
+    public void attack() throws Exception {
+
+    }
+
+}
